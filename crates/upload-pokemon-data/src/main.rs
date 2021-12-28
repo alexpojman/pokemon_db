630ff0ac2b47e7060fe3af71b8ab56bf7358119d
Alex Pojman <pojman.alex@gmail.com>
2021-12-28 15:05:34 -0600

    Add database types and ksuid

--- crates/upload-pokemon-data/src/main.rs
+++ crates/upload-pokemon-data/src/main.rs
@@ -1,5 +1,7 @@
+mod db;
 mod pokemon_csv;
 use pokemon_csv::*;
+use db::*;
 
 fn main() -> Result<(), csv::Error> {
     let mut rdr = csv::Reader::from_path("./crates/upload-pokemon-data/pokemon.csv")?;
